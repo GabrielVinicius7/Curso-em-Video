ea54bad1a59d7a6902786688ba72702171c04a12
Gabriel <gvsilva7767@gmail.com>
2023-03-22 20:54:12 -0400

    Até o exercicio 63

---
+++ exercicio 63.py
@@ -0,0 +1,26 @@
+# Sequência de Fibonacci
+
+
+
+print('-'*33)
+print('     Sequência de Fibonacci    ')
+print('-'*33)
+
+f = int(input('Quantos termos você quer mostrar ?'))
+
+t1 = 0
+t2 = 1
+
+cont = 0
+
+print('{} -> {} -> '.format(t1,t2),end="")
+
+while cont <= f:
+    t3 = t1 + t2
+    print('{} ->'.format(t3),end=" ")
+    t1 = t2
+    t2 = t3
+
+    cont += 1
+
+print(' Fim.')
